0a2a349cbe6e92b68c8ba5f41b49b0b50212038f
Magnus Nilsson <regnhundar@gmail.com>
2024-03-11 14:14:04 +0100

    inväntar css/html

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,44 @@
 import apiModule from "./apiModule.js";
+
+window.addEventListener(`DOMContentLoaded`, () => {
+    // Eventlystnare osv här inne.
+    populateMenu()
+})
+
+async function populateMenu() {
+    try {
+        const data = await apiModule.getData(`https://santosnr6.github.io/Data/airbeanproducts.json`);
+        const menu = data.menu;
+
+        const bodyRef = document.querySelector(`body`);
+
+        menu.forEach(coffee => {
+            const coffeeWrapperRef = document.createElement(`div`);
+
+            const buyButtonRef = document.createElement(`button`);
+            // sendToCart existerar inte än 
+            // buyButtonRef.addEventListener(`click` sendToCart); 
+            buyButtonRef.textContent = `+`;
+            coffeeWrapperRef.appendChild(buyButtonRef);
+
+            const coffeeTitleRef = document.createElement(`h3`);
+            coffeeTitleRef.textContent = coffee.title;
+            coffeeWrapperRef.appendChild(coffeeTitleRef);
+            
+            const coffeePriceRef = document.createElement(`p`);
+            coffeePriceRef.textContent = `${coffee.price} kr`;
+            coffeeWrapperRef.appendChild(coffeePriceRef);
+
+            const coffeeDescriptionRef = document.createElement(`p`);
+            coffeeDescriptionRef.textContent = coffee.desc;
+            coffeeWrapperRef.appendChild(coffeeDescriptionRef);
+
+            bodyRef.appendChild(coffeeWrapperRef);
+
+        });
+    
+        
+    } catch (error) {
+        console.log(`Error at populateMenu ` + error);
+    }
+}
