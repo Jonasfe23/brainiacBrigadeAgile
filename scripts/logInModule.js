2990c4195866087786b0cf8efe392e35e59d4d55
Rindertanijk1997 <yhfe23.van.ri@folkuniversitetet.nu>
2024-03-13 14:46:47 +0100

    funcUserOrAdmin done

--- scripts/logInModule.js
+++ scripts/logInModule.js
@@ -44,3 +44,86 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+
+
+
+
+
+
+ 
+
+document.addEventListener('DOMContentLoaded', async () => { 
+
+    const loginForm = document.getElementById('loginForm'); 
+
+     
+
+    // Lyssnar på formulärets inlämning 
+
+    loginForm.addEventListener('submit', async event => { 
+
+        event.preventDefault(); // Förhindra standardbeteendet för formulär 
+
+         
+
+        // Hämtar användarnamn och lösenord från formuläret 
+
+        const username = loginForm.querySelector('input[name="username"]').value; 
+
+        const password = loginForm.querySelector('input[name="password"]').value; 
+
+         
+
+        try { 
+
+            // Hämtar användarinformation från JSON-filen 
+
+            const response = await fetch('https://santosnr6.github.io/Data/airbeanusers.json'); 
+
+            const userData = await response.json(); 
+
+             
+
+            // Söker efter en matchande användare i användarlistan 
+
+            const user = userData.users.find(user => user.username === username && user.password === password); 
+
+             
+
+            if (user) { 
+
+                // Sparar användarnamnet i localStorage för att indikera att användaren är inloggad 
+
+                localStorage.setItem('loggedInUser', username); 
+
+                const userRole = user.role; 
+
+                console.log('Användarroll:', userRole); 
+
+                 
+
+                if (userRole === 'admin') { 
+
+                    // Visar redigeringsknappen för admin-användare 
+
+                    const editMenuBtn = document.querySelector('.edit-menu-btn'); 
+
+                    if (editMenuBtn) editMenuBtn.classList.remove('d-none'); 
+
+                } 
+
+            } else { 
+
+                console.error('Fel användarnamn eller lösenord'); 
+
+            } 
+
+        } catch (error) { 
+
+            console.error('Fel vid inloggning:', error); 
+
+        } 
+
+    }); 
+
+}); 
